ff7d94d2c5fec43eb241bb82abffe32afb0a7ba1
zoeludena <88351041+zoeludena@users.noreply.github.com>
2021-08-16 23:07:13 +0000

    We finished mirroring the image!

--- main.py
+++ main.py
@@ -1,7 +1,77 @@
-print("Running lab05Warmup_Rena.py") # let us know it's running lab05Warmup_Felix.py
+#print("Running lab05Warmup_Rena.py") # let us know it's running lab05Warmup_Felix.py
 
-import lab05Warmup_Rena              # this will cause lab05Warmup_Felix.py to run
+#import lab05Warmup_Rena              # this will cause lab05Warmup_Felix.py to run
 
-print("Running lab05Warmup_Zoe.py")  # let us know it's running lab05Warmup_Ryan.py
+#print("Running lab05Warmup_Zoe.py")  # let us know it's running lab05Warmup_Ryan.py
 
-import lab05Warmup_Zoe               # this will cause lab05Warmup_Ryan.py to run
+#import lab05Warmup_Zoe               # this will cause lab05Warmup_Ryan.py to run
+
+#Gray Scale:
+from PIL import Image
+
+bear = Image.open( "bear.png" )
+
+#def grayscale(im):
+  
+ #(width, height) = im.size
+
+ #We want to be able to change the whole image or just part of an image
+
+ #for x in range(width):
+  #for y in range(height):
+     #We used the same idea from the invert() in the warmup, but tried to make change based on the grayscale
+    #(red, green, blue) = im.getpixel((x, y))
+      
+    #luminance = int(0.21*int(red) + 0.72*int(green)+ 0.07 *int(blue))
+    #the luminance value that the colors, RGB, get modified by
+    #bear.putpixel((x,y), (luminance, luminance, luminance))
+     #(#,#,#) r b g
+     #each color channel for every pixel is modified by the luminance value
+#grayscale(bear)
+#bear.save("bear1.png")
+
+#binarize:
+#def binarize(im, thresh, startx, starty, endx, endy):
+  #(width, height) = im.size
+
+  #for x in range(startx, endx):
+    #for y in range(starty, endy):
+      #(red, green, blue) = im.getpixel((x,y))
+      
+      #luminance = int(0.21*int(red) + 0.72*int(green)+ 0.07 *int(blue))
+      #We still need the luminance because we want to be able to determine how bright each pixel is
+
+      #if luminance > thresh:
+        #bear.putpixel((x,y), (255, 255, 255))
+        #We want anything above the threshhold to be white
+      #else:
+        #bear.putpixel((x,y), (0,0,0))
+        #We want anything below the threshhold to be black
+#binarize(bear, 127, 0, 0, 600, 800)
+#bear.save("bear2.png")
+
+#Vertical Mirroring
+
+#def mirrorVert(im):
+  #We want to cut the image in half (y axis) and copy each pixel from above to below
+ #(width, height) = bear.size
+
+ #for x in range(width):
+  #for y in range(int(height/2)):
+   #(red, green, blue) = bear.getpixel((x,y))
+   #bear.putpixel((x, height-y-1), (red, green, blue))
+  
+#mirrorVert(bear.png)
+#bear.save("bear3.png")
+
+#def mirrorHoriz(im):
+  #We want to cut the image in half (x axis) and copy each pixel from above to below
+ #(width, height) = bear.size
+
+ #for x in range(int(width/2)):
+  #for y in range(height):
+   #(red, green, blue) = bear.getpixel((x,y))
+   #bear.putpixel((width-x-1, y), (red, green, blue))
+  
+#mirrorHoriz(bear.png)
+#bear.save("bear4.png")
