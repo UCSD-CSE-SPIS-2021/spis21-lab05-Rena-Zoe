b5509d36b84f06866306f3d8568c7447fbc4b6fd
Rena2025 <88347346+Rena2025@users.noreply.github.com>
2021-08-20 20:30:52 +0000

    We finished lab5. Zoe is awesome!!

--- main.py
+++ main.py
@@ -101,3 +101,31 @@ def blur(im):
 
 blurbear = blur(bear)
 blurbear.save("BlurBearEdited.png")
+
+#Random Grid
+
+import random.shuffle
+
+def grid(im, n=25):
+  #Giving image and number
+    #They can set n to something else in their parameter, but for default this would make it split by 25
+
+  #Convenient constants
+  (width, height) = im.size
+  num_blocks_x = width//n
+  num_blocks_y = height//n
+
+  #Shuffle input blocks to get output blocks
+  blocks_nums = list(range(num_blocks_x*num_blocks_y))
+  random.shuffle(in_block_nums)
+  #We can then randomly shuffle the list to tell us where to copy over
+  temp = Image.new('RGB', (width,height))
+
+  #For each block of output, copy from input
+  for out_block_num in range(num_blocks_x*num_blocks_y):
+    in_block_nums = in_block_nums[out_block_num]
+    #copy inblocknum to the corresponding output block
+  return
+
+  #Number of vertical blocks you have is height//n
+    #This splits up how many blocks height-wise you will have, and you can do the same with width
